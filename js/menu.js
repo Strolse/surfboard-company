7c58f732661e37e5b99330af3c416da331966e5f
Olga Strekalovskaia <afalina_91@mail.ru>
2021-12-12 19:38:03 +0300

    добавила полноэкранное меню

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+const openMenu = document.querySelector("#hamburger");
+const fullscreenMenu = document.querySelector("#fullscreen-menu");
+const menu = document.querySelector("#menu");
+const close = document.querySelector("#fullscreen-menu__close");
+
+openMenu.addEventListener("click", function(event){
+  event.preventDefault();
+  fullscreenMenu.style.display = "block";
+  menu.style.display = "block";
+  console.log(fullscreenMenu);
+})
+
+close.addEventListener("click", function(event){
+  event.preventDefault;
+  fullscreenMenu.style.display = "none";
+  menu.style.display = "none";
+})
