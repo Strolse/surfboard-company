4702665c4d1cb2a9663b2af7e63ab868af94b033
Olga Strekalovskaia <afalina_91@mail.ru>
2021-12-20 01:06:01 +0300

    реализовала слайдер

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,14 @@
-// $('.products').bxSlider({
-//   preloadImages:'all'
-// }
+const slider = $('.products').bxSlider({
+  controls: false,
+  pager: false,
+});
 
+$('#arrow-next').on('click', (e) =>{
+  e.preventDefault();
+  slider.goToNextSlide();
+});
 
-// );
+$('#arrow-prev').on('click', (e) =>{
+  e.preventDefault();
+  slider.goToPrevSlide();
+})
