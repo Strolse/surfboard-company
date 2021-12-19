7ce8a9f36006a7b1db38f6918949ec5c59a0e18e
Olga Strekalovskaia <afalina_91@mail.ru>
2021-12-19 23:56:43 +0300

    завершила работу по отправке запроса в форме(валидация, обработка ответа)

--- js/modal.js
+++ js/modal.js
@@ -1,3 +1,17 @@
+const validateFields = (form, fieldsArray)=>{
+  fieldsArray.forEach(elem =>{
+
+    elem.removeClass('error');
+    if(elem.val().trim() == ""){
+      elem.addClass('error');
+    }
+  })
+
+  const withError = form.find('.error');
+
+  return withError.length == 0;
+}
+
 $('.form').on('submit', e =>{
   e.preventDefault();
 
@@ -6,26 +20,48 @@ $('.form').on('submit', e =>{
   const phone = form.find("[name='phone']");
   const comment = form.find("[name='comment']");
   const to = form.find("[name='to']");
+  const modal = $('#modal');
+  const modalText = modal.find('.modal__text');
 
-  $.ajax({
-    url: "https://webdev-api.loftschool.com/sendmail",
-    method: "post",
-    data: {
-      name: name.val(),
-      phone: phone.val(),
-      comment: comment.val(),
-      to: to.val(),
-    },
-  })
+  modalText.removeClass('error');
 
-  // $.fancybox.open({
-  //   src: "#modal",
-  //   type: "inline",
-  //   smallBtn: false
-  // })
+
+  const isValid = validateFields(form, [name, phone, comment, to]);
+
+    if(isValid) {
+    const request = $.ajax({
+      url: "https://webdev-api.loftschool.com/sendmail",
+      method: "post",
+      data: {
+        name: name.val(),
+        phone: phone.val(),
+        comment: comment.val(),
+        to: to.val(),
+      }
+    })
+    
+    request.done(data =>{
+      modalText.text(data.message);
+    })
+
+    request.fail(
+      data =>{
+        const errorText = data.responseJSON.message;
+        modalText.text(errorText);
+        modalText.addClass('error');
+      })
+
+      request.always(
+        $.fancybox.open({
+          src: "#modal",
+          type: "inline",
+          smallBtn: false
+        })
+      )
+  }
 })
 
-// $(".app-modal-close").on('click', e =>{
-//   e.preventDefault();
-//   $.fancybox.close();
-// })
+$(".app-modal-close").on('click', e =>{
+  e.preventDefault();
+  $.fancybox.close();
+})
