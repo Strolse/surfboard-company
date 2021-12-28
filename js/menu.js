bcc902cb239a6afd16c2ba3e092a74bf0c5452db
Olga Strekalovskaia <afalina_91@mail.ru>
2021-12-28 19:09:52 +0300

    сделала рабочими сылки в меню-гамбургер

--- js/menu.js
+++ js/menu.js
@@ -3,6 +3,7 @@ const fullscreenMenu = document.querySelector("#fullscreen-menu");
 const menu = document.querySelector("#menu");
 const close = document.querySelector("#fullscreen-menu__close");
 
+
 openMenu.addEventListener("click", function(event){
   event.preventDefault();
   fullscreenMenu.style.display = "block";
@@ -16,4 +17,12 @@ close.addEventListener("click", function(event){
   menu.style.display = "none";
 })
 
+menu.addEventListener('click', function(e){
+  e.preventDefault;
 
+
+  if(e.target.classList.contains("menu__link")){
+    fullscreenMenu.style.display = "none";
+    menu.style.display = "none";
+  }
+})
